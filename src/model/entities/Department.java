e50d4cef25e48d0620c96618115aa5e4beeb7ff1
Keicia Nolasco <keicianolasco@hotmail.com>
2024-01-06 11:05:56 -0300

    Created the Classes (DB, DbException, DbIntegrityException, DaoFactory, DepartmentDaoJDBC, SellerDaoJDBC and Seller), Created the Interfaces (DepartmentDao and SellerDao) and Updated the Class (DepartmentService)

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -8,7 +8,7 @@ public class Department implements Serializable {
 
 	private Integer id;
 	private String name;
-
+	
 	public Department() {
 	}
 
@@ -53,8 +53,7 @@ public class Department implements Serializable {
 		if (id == null) {
 			if (other.id != null)
 				return false;
-		} 
-		else if (!id.equals(other.id))
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
 	}
@@ -63,5 +62,4 @@ public class Department implements Serializable {
 	public String toString() {
 		return "Department [id=" + id + ", name=" + name + "]";
 	}
-	
 }
